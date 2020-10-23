1a552a31016e770eed68bc084af42ff686fe95e3
Theo <theodore.leonardo@student.umn.ac.id>
2020-10-23 21:52:29 +0700

    asd

---
+++ app/Http/Controllers/GaunController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gaun;
+use Illuminate\Http\Request;
+
+class GaunController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Gaun  $gaun
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Gaun $gaun)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Gaun  $gaun
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Gaun $gaun)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Gaun  $gaun
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Gaun $gaun)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Gaun  $gaun
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Gaun $gaun)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/admin', function () {
+    return view('admin');
 });
+
+Route::get('/contact', [ContacController::class, 'index']);
+Route::get('/ukurangaun', [UkurangaunController::class, 'index']);
+Route::get('/baner', [BanerController::class, 'index']);
+Route::get('/gaun', [GaunController::class, 'index']);
